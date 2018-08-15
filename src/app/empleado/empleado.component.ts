bbb7805baf9d89595ac74f39050289108105ca90
JonatanCS <jcastellanos.nc@gmail.com>
2018-08-15 10:38:09 +0200

    first git push

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'empleado-tag',
+    templateUrl: './empleado.component.html'
+})
+
+export class EmpleadoComponent{
+    public titulo = 'Título del componente de empleado';
+}
