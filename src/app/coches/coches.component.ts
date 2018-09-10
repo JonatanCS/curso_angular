be108a62b778ae28e3b93b9589b5fef53b1da467
JonatanCS <jcastellanos.nc@gmail.com>
2018-09-10 19:06:32 +0200

    form tutorial completed

--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -8,12 +8,18 @@ import { Coche } from './coche';
 
 export class CochesComponent{
     public coche: Coche;
+    public coches: Array<Coche>;
 
     constructor(){
         this.coche = new Coche("","","");
+        this.coches = [
+            new Coche("Seat","52","azul"),
+            new Coche("Citroen","100","gris")
+        ];
     }
 
     onSubmit(){
-        console.log(this.coche);
+        this.coches.push(this.coche);
+        this.coche = new Coche("","","");
     }
 }
